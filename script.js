9c2dbb922317047558bd0426baea2723cc16a460
ThiagoOliveiraCordeiro <thiagoty52@gmail.com>
2022-07-01 13:24:26 -0300

    feat: pizza flavor display, pizza description and image

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const selectAll = (element) => document.querySelectorAll(element);
 pizzaJson.map((item, index) => {
   let pizzaItem = select('.models .pizza-item').cloneNode(true);
 
+  pizzaItem.setAttribute('data-key', index);
+
   pizzaItem.querySelector('.pizza-item--img img').src = item.img;
   pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
   pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
@@ -11,6 +13,13 @@ pizzaJson.map((item, index) => {
 
   pizzaItem.querySelector('a').addEventListener('click', (event) => {
     event.preventDefault();
+
+    let key = event.target.closest('.pizza-item').getAttribute('data-key');
+
+    select('.pizzaBig img').src = pizzaJson[key].img;
+    select('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+    select('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+    
     
     select('.pizzaWindowArea').style.display.opacity = 0;
     select('.pizzaWindowArea').style.display = 'flex';
