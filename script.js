dc225adf77173d3b79ca95e60c19cb8ebc7663d3
ThiagoOliveiraCordeiro <thiagoty52@gmail.com>
2022-07-01 10:05:28 -0300

    feat: Taking the data and listing the pizzas

---
+++ script.js
@@ -0,0 +1,13 @@
+const select = (element) => document.querySelector(element);
+const selectAll = (element) => document.querySelectorAll(element);
+
+pizzaJson.map((item, index) => {
+  let pizzaItem = select('.models .pizza-item').cloneNode(true);
+
+  pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+  pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+  pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+  pizzaItem.querySelector('.pizza-item--desc').innerHTML =  item.description;
+
+  select('.pizza-area').append(pizzaItem);
+});
